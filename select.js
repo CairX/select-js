1bf89b94cc3ae2d75d0b8ce4592b1f301e42f1cf
CairX <lazycairx@gmail.com>
2017-05-22 18:47:14 +0200

    Append CSS classes to elements and add styling

--- select.js
+++ select.js
@@ -1,32 +1,50 @@
+/* global ClassName */
+
 "use strict";
 
 var Select = (function() {
 	var create = function(select) {
-		var header = markupHeader(select.getAttribute("placeholder"));
+		var container = markupContainer();
 		var drop = markupDrop();
-
+		var header = markupHeader(drop, select.getAttribute("placeholder"));
 		select.querySelectorAll("option").forEach(function(option, index) {
-			var item = markupOption(option, index);
+			var item = markupOption(select, option, index);
 			drop.appendChild(item);
 		});
 
 		// Insert markup into the DOM.
-		select.insertAdjacentElement("afterend", header);
-		header.insertAdjacentElement("afterend", drop);
+		select.insertAdjacentElement("afterend", container);
+		container.appendChild(header);
+		container.appendChild(drop);
+
+		// Hide the original select.
+		ClassName.unique(select, "select-hide");
 	};
 
-	var markupHeader = function(text) {
-		var header = document.createElement("div");
+	var markupContainer = function() {
+		var container = document.createElement("div");
+		container.setAttribute("class", "select-container");
+		return container;
+	};
+
+	var markupHeader = function(drop, text) {
+		var header = document.createElement("button");
+		header.setAttribute("class", "select-header");
 		header.innerHTML = text;
+		header.addEventListener("click", function() {
+			toggleDrop(drop);
+		});
+		toggleDrop(drop);
 		return header;
 	};
 
 	var markupDrop = function() {
 		var drop = document.createElement("ul");
+		drop.setAttribute("class", "select-drop");
 		return drop;
 	};
 
-	var markupOption = function(option, index) {
+	var markupOption = function(select, option, index) {
 		var li = document.createElement("li");
 
 		var label = document.createElement("label");
@@ -37,7 +55,9 @@ var Select = (function() {
 		input.checked = option.selected;
 		input.setAttribute("type", "checkbox");
 		input.setAttribute("data-index", index + 1);
-		input.addEventListener("change", updateOption);
+		input.addEventListener("change", function(event) {
+			updateOption(select, event.target);
+		});
 
 		var text = document.createTextNode(option.innerText);
 		label.appendChild(text);
@@ -45,10 +65,18 @@ var Select = (function() {
 		return li;
 	};
 
-	var updateOption = function(event) {
-		var option = select.querySelector("option:nth-child(" + event.target.getAttribute("data-index") + ")");
-		option.selected = event.target.checked;
-		if (event.target.checked) {
+	var toggleDrop = function(drop) {
+		if (ClassName.has(drop, "select-hide")) {
+			ClassName.remove(drop, "select-hide");
+		} else {
+			ClassName.append(drop, "select-hide");
+		}
+	};
+
+	var updateOption = function(select, checkbox) {
+		var option = select.querySelector("option:nth-child(" + checkbox.getAttribute("data-index") + ")");
+		option.selected = checkbox.checked;
+		if (checkbox.checked) {
 			option.setAttribute("selected", "");
 		} else {
 			option.removeAttribute("selected");
@@ -60,5 +88,6 @@ var Select = (function() {
 	};
 })();
 
-var select = document.querySelector("select");
-Select.create(select);
+var selects = document.querySelectorAll("select");
+Select.create(selects[0]);
+Select.create(selects[1]);
